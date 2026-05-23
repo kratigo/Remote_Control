d356c5a9b5ad4169a434a05021859895837d6ebe
Evgeniy <betonxg5@gmail.com>
2026-05-23 12:36:50 +0300

    скрыл данные о боте

--- CS_GameTime/AppConfig.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CS_GameTime
-{
-    internal class AppConfig
-    {
-        public static long MY_userId = 1816585045;
-        public static string Token = "7901504622:AAHv3f8CgqNZyutZ9yQhFvRTIvenkqn9-JU";
-    }
-}
